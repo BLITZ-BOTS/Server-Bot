1e8f8fb713f993c3b6bae6f87ea5d3ac444c2c90
Char <char@charcodes.online>
2024-12-16 18:31:45 +0000

    Added welcome msg

--- plugins/welcome_role/events/join.ts
+++ plugins/welcome_role/events/join.ts
@@ -1,12 +1,15 @@
-import { Client, GuildMember } from "npm:discord.js";
+import { Client, GuildMember, TextChannel } from "npm:discord.js";
 export default {
   event: "guildMemberAdd",
   once: false,
   action: (
-    _client: Client,
-    config: { role_id: string },
+    client: Client,
+    config: { role_id: string, welcome_channel: string },
     member: GuildMember,
   ) => {
     member.roles.add(config.role_id);
+    const channel = client.channels.cache.get(config.welcome_channel) as TextChannel;
+    if (!channel) return;
+    channel.send(`Welcome to the server <@${member.id}>!`)
   },
 };
